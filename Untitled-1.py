baac4b695d2c4539dfc6f9f94bf79448824a553d
RANKblank36 <tendosana@gmail.com>
2025-02-13 18:12:33 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "division by zero is not allowed"
+    return a / b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("division:", divide(num1, num2))
